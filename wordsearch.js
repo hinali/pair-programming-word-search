dc3270130237e15bd16b1e20f7101d8ef3395aa6
hinali <hinali030@gmail.com>
2023-08-30 20:21:01 -0600

    Pair Programming - Word Search with @brett

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+//                   [array]/"WORD"
+const wordSearch = (letters, word) => {
+    // Sort -> Vertical
+    let results = [];
+    for (let col = 0; col < letters[0].length; col++) {
+      const newArray = [];
+      for (let row = 0; row < letters.length; row++) {
+        newArray.push(letters[row][col]);
+      }
+      results.push(newArray);
     }
-}
-
-module.exports = wordSearch
+    // console.log("results:", results);
+    // Check Vertical
+    const verticalJoin = results.map(ls => ls.join(''));
+    for (const l of verticalJoin) {
+      if (l.includes(word)) {
+        return true;
+      }
+    }
+    // Check Horizontal
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (const l of horizontalJoin) {
+      if (l.includes(word)) {
+        return true;
+      }
+    }
+    return false;
+    
+  };
+  
+  
+  module.exports = wordSearch;
